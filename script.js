d89bdda12f2053ac2191ac3fc1e8f8fa408163e2
Andrejsd2010 <114296105+Andrejsd2010@users.noreply.github.com>
2022-10-08 15:26:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var buttonElement = document.getElementById('dugme')
+
+buttonElement.addEventListener('click',function(){
+    alert('Kliknuto je dugme!');
+
+    for(i = 0;i<3;i++){
+        alert('broj' + i);
+    }
+
+});
